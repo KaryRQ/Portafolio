8e3a8aeaae6dfc31d2399641aeb5fb6507021156
KaryRQ <darkness.kary@hotmail.com>
2020-07-08 19:54:09 -0500

    actualizacion

--- js/main.js
+++ js/main.js
@@ -107,7 +107,7 @@ var typed = $(".typed");
 
   $(function() {
     typed.typed({
-      strings: ["Kary Quezada", "Ux Designer"],
+      strings: ["Kary Quezada", "Ux Researcher"],
       typeSpeed: 100,
       loop: true,
     });
